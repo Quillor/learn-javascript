6b1499c7ebbab1395eb99faae2c18d906edade0c
Quillor <info@quillor.com>
2018-07-09 21:57:10 -0400

    Init

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+// alert("hello");
+// alert('Hello World!');
+console.log('Secret message');
+// document.write("Document Write");
+
+var newUserProfile = function () {
+  // click on the register
+}
+
+
+var username = "Jane";
+username = "Billy";
+
+var price = 2.5;
+
+
+
+function priceOfCookies (a) {
+  var numberOfCookies = prompt ("How how many cookies would you like?");
+  return numberOfCookies * price;
+}
+
+alert("The cookies are $" + priceOfCookies(1) + ".00" );
+// sendAlert(username);
+
+// alert(username);
