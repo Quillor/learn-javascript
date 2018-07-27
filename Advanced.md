a5e27dcd8a18cfb91cf4ca5df70138139cab573e
Quillor <info@quillor.com>
2018-07-26 20:17:33 -0400

    Made slideshow logic

--- Advanced.md
+++ Advanced.md
@@ -33,3 +33,68 @@ let name
 name = "Tim"
 
 ```
+
+
+
+## Hoisting
+When a local variable get
+### this is hoisting
+```JS
+// Hoisting
+function outer() {
+   let a = "hello"
+   inner = function () {
+     console.log("Inner function called")
+   }
+}
+
+outer()
+
+// This can be called, because the outer() function has "hoisted" inner() to
+// the global scope
+inner()
+```
+### this is NOT hoisting
+```JS
+// Hoisting
+function outer() {
+   let a = "hello"
+   inner = function () {
+     console.log("Inner function called")
+   }
+}
+// This can NOT be called, because the outer() function has NOT been called inner()
+inner()
+```
+
+
+## Using Objects and functions together
+
+```JS
+slideShow = {
+  photoList: ["1.pet.png", "2.selfie.png", "3.mountain.png"],
+  currentPhotoIndex: 0,
+  nextPhoto: function (times) {
+    this.currentPhotoIndex++
+    console.log("The photo has moved up to " + this.photoList[this.currentPhotoIndex]);
+  },
+  prevPhoto: function () {
+    this.currentPhotoIndex--
+    console.log("The photo has moved back to " +  this.photoList[this.currentPhotoIndex]);
+  },
+  getCurrentPhoto: function () {
+    console.log("The photo is currently " + this.photoList[this.currentPhotoIndex])
+  }
+}
+slideShow.getCurrentPhoto()
+slideShow.nextPhoto()
+slideShow.prevPhoto()
+slideShow.nextPhoto()
+```
+
+``` console
+The photo is currently 1.pet.png
+The photo has moved up to 2.selfie.png
+The photo has moved back to 1.pet.png
+The photo has moved up to 2.selfie.png
+```
--- js/lesson2-advanced.js
+++ js/lesson2-advanced.js
@@ -14,24 +14,63 @@
 // console.log(name)
 
 
+// Scope
+// function outer() {
+//
+//   let a = "Bird scooter"
+//   let b = {
+//     type:   "Fiat",
+//     model:  "500",
+//     color:  "white"
+//   }
+//   console.log(`First: I ride a ${a} and a ${b.type}`)
+//
+//   function inner(a, b) {
+//     console.log(`Second: I ride a ${a} and a ${b.type}`)
+//      a = "Razor scooter"
+//      b = 4
+//     console.log(`Lastly, I ride a ${b} ${a}s `)
+//   }
+//   inner(a, b)
+//
+// }
+// outer()
 
-function outer() {
+// // Hoisting
+// function outer() {
+//    let a = "hello"
+//    inner = function () {
+//      console.log("Inner function called")
+//    }
+// }
+//
+// outer()
+//
+// // This can be called, because the outer() function has "hoisted" inner() to
+// // the global scope
+// inner()
 
-  let a = "Bird scooter"
-  let b = {
-    type:   "Fiat",
-    model:  "500",
-    color:  "white"
+
+
+
+slideShow = {
+  photoList: ["1.pet.png", "2.selfie.png", "3.mountain.png"],
+  currentPhotoIndex: 0,
+  nextPhoto: function () {
+    this.currentPhotoIndex++
+    console.log("The photo has moved up to " + this.photoList[this.currentPhotoIndex]);
+  },
+  prevPhoto: function () {
+    this.currentPhotoIndex--
+    console.log("The photo has moved back to " +  this.photoList[this.currentPhotoIndex]);
+  },
+  getCurrentPhoto: function () {
+    console.log("The photo is currently " + this.photoList[this.currentPhotoIndex])
   }
-  console.log(`First: I ride a ${a} and a ${b.type}`)
-
-  function inner(a, b) {
-    console.log(`Second: I ride a ${a} and a ${b.type}`)
-     a = "Razor scooter"
-     b = 4
-    console.log(`Lastly, I ride a ${b} ${a}s `)
-  }
-  inner(a, b)
-
 }
-outer()
+
+// console.log( `First, I have a photo of my ${slideShow.getCurrentPhoto()}` );
+slideShow.getCurrentPhoto()
+slideShow.nextPhoto()
+slideShow.prevPhoto()
+slideShow.nextPhoto()
